f9836b31a1c539fe872341f844e2e9eba361e70b
Fletcher Watson <fbwthe6@gmail.com>
2017-02-07 14:44:43 -0600

    stubbed out xhr request. returns large json object. need only specific parts.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,2 @@
-
+let tempEl = document.querySelector('.temp')
+tempEl.innerHTML = get(`http://api.wunderground.com/api/${WUKey}/conditions/q/TN/Nashville.json`)
--- src/scripts/requests.js
+++ src/scripts/requests.js
@@ -1,17 +1,34 @@
-let xhr = new XMLHttpRequest();
-xhr.open('GET', `http://api.wunderground.com/api/${WUKey}/conditions/q/TN/Nashville.json`);
-xhr.send(null);
+function get(url) {
+  // Return a new promise.
+  return new Promise(function(resolve, reject) {
+    // Do the usual XHR stuff
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
 
-xhr.onreadystatechange = function () {
-  const DONE = 4 // readyState 4 means the request is done.
-  const OK = 200 // status 200 is a successful return.
-  if (xhr.readyState === DONE) {
-    if (xhr.status === OK){
-      let data = xhr.responseText
-      console.log(data); // 'This is the returned text.'
-    }
-  } else {
-    let status = xhr.status
-    console.log('Error: ' + status); // An error occurred during the request.
-  }
+    xhr.onload = function() {
+      // This is called even on 404 etc
+      // so check the status
+      if (xhr.status == 200) {
+        // Resolve the promise with the response text
+        console.log(xhr.responseText)
+        resolve(xhr.response);
+      }
+      else {
+        // Otherwise reject with the status text
+        // which will hopefully be a meaningful error
+
+        reject(Error(xhr.statusText));
+      }
+    };
+
+    // Handle network errors
+    xhr.onerror = function() {
+      reject(Error("Network Error"));
+    };
+
+    // Make the request
+    xhr.send();
+  });
 }
+
+// get(`http://api.wunderground.com/api/${WUKey}/conditions/q/TN/Nashville.json`)
